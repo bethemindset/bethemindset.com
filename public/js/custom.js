68941d1e63df7c91265de545a173e7c2f8d72711
Paul Borawski <paulborawski14@gmail.com>
2016-11-30 10:04:02 -0800

    disappearing navbar

---
+++ public/js/custom.js
@@ -0,0 +1,12 @@
+var navBackground = document.getElementById('testing123')
+var navFontColor = document.querySelector('.testy1')
+var navFontColor2 = document.querySelector('.testy2')
+var navFontColor3 = document.querySelector('.testy3')
+var navFontColor4 = document.querySelector('.testy4')
+window.onscroll = function (e) {
+  navBackground.style.backgroundColor = '#ffffff'
+  navFontColor.style.color = '#000000'
+  navFontColor2.style.color = '#000000'
+  navFontColor3.style.color = '#000000'
+  navFontColor4.style.color = '#000000'
+}
