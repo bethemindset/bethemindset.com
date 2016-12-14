1c70750338d8d345510bc456d1082caac3312946
Paul Borawski <paulborawski14@gmail.com>
2016-12-13 20:16:10 -0800

    different nav color

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,11 +4,11 @@ var navFontColor2 = document.querySelector('.testy2')
 var navFontColor3 = document.querySelector('.testy3')
 var navFontColor4 = document.querySelector('.testy4')
 window.onscroll = function (e) {
-  navBackground.style.backgroundColor = '#ffffff'
-  navFontColor.style.color = '#000000'
-  navFontColor2.style.color = '#000000'
-  navFontColor3.style.color = '#000000'
-  navFontColor4.style.color = '#000000'
+  navBackground.style.backgroundColor = '#000000'
+  navFontColor.style.color = '#ffffff'
+  navFontColor2.style.color = '#ffffff'
+  navFontColor3.style.color = '#ffffff'
+  navFontColor4.style.color = '#ffffff'
 }
 
 $(window).scroll(function () {
