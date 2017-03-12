1a81e8c8955cdfb98b7e90db049a56556c7a1cca
Paul Borawski <paulborawski14@gmail.com>
2017-03-11 19:37:11 -0800

    made function to generate new student parameters/opts

--- public/homework/homework.js
+++ public/homework/homework.js
@@ -1,61 +1,64 @@
-/* Object creation of random student */
-function Student(name, homework, quiz, exam, scores, homeworkAverage, quizAverage, examAverage, classAverage, gpa) {
-  this.name                 = name
-  this.homework             = homework;
-  this.quiz                 = quiz;
-  this.exam                 = exam
-  this.scores               = scores
-  this.homeworkAverage      = homeworkAverage
-  this.quizAverage          = quizAverage
-  this.examAverage          = examAverage
-  this.classAverage         = classAverage
-  this.gpa                  = gpa
-}
+
 
 /*Generates a random student on button click*/
 var buttonClick = document.querySelector('.randomStudentHTML')
 buttonClick.addEventListener('click', function(event) {
-  var randomStudent = new Student("RandomName", homework, quiz, exam, scores, homeworkAverage)
+  var randomStudent = new Student ( makeOpts() )
   console.log(randomStudent)
 })
 
-/* HOMEWORK */
-
-homework = []
-//random number from 3-5
-var threeToFive = Math.floor(Math.random() * 3) + 3
-//3-5 grades from 0-100
-  for (var i = 0; i < threeToFive; i++) {
-    homework.push(Math.floor(Math.random() * 101))
-  }
-
-/* HOMEWORK AVERAGE */
-
-homeworkAverage = []
-
-var total = 0;
-for(var i = 0; i < homework.length; i++) {
-    total += homework[i];
-}
-var homeworkAvg = total / homework.length;
-homeworkAverage.push(homeworkAvg)
-
-/* QUIZ */
-
-quiz = []
-//random number from 0-3
-var zeroToThree = Math.floor(Math.random() * 4)
-//0-3 grades from 0-100
-  for (var i = 0; i < zeroToThree; i++) {
-    quiz.push(Math.floor(Math.random() * 101))
+function Student ( opts ) {
+  this.name                 = opts.name || 'paul'
+  this.homework             = opts.homework || 'default'
+  this.quiz                 = opts.quiz || 'default'
+  this.exam                 = opts.exam || 'default'
+  this.scores               = opts.scores || 'default'
+  this.homeworkAverage      = opts.homeworkAverage || 'default'
+  this.quizAverage          = opts.quizAverage || 'default'
+  this.examAverage          = opts.examAverage || 'default'
+  this.classAverage         = opts.classAverage || 'default'
+  this.gpa                  = opts.gpa || 'default'
 }
 
-/* EXAM */
+function makeOpts(){
+  /* Object creation of random student */
+  var opts = {}
 
-exam = []
-//random number from 1-2
-var oneToTwo = Math.floor(Math.random() * 2) + 1
-//1-2 grades from 0-100
-  for (var i = 0; i < oneToTwo; i++) {
-    exam.push(Math.floor(Math.random() * 101))
+  /* HOMEWORK */
+
+  opts.homework = []
+  //random number from 3-5
+  var threeToFive = Math.floor(Math.random() * 3) + 3
+  //3-5 grades from 0-100
+  while(threeToFive--)
+    opts.homework.push(Math.floor(Math.random() * 101))
+
+  /* HOMEWORK AVERAGE */
+
+  opts.homeworkAverage = []
+
+  var total = 0, l = opts.homework.length;
+  while(l--)
+    total += opts.homework[l]
+  opts.homeworkAverage.push(total / opts.homework.length)
+
+  /* QUIZ */
+
+  opts.quiz = []
+  //random number from 0-3
+  var zeroToThree = Math.floor(Math.random() * 4)
+  //0-3 grades from 0-100
+  while (zeroToThree--)
+    opts.quiz.push(Math.floor(Math.random() * 101))
+
+  /* EXAM */
+
+  opts.exam = []
+  //random number from 1-2
+  var oneToTwo = Math.floor(Math.random() * 2) + 1
+  //1-2 grades from 0-100
+  while (oneToTwo--)
+    opts.exam.push(Math.floor(Math.random() * 101))
+
+  return opts
 }
